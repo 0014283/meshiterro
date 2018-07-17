24691781e28dc41924947251c4d9ea3c3fdae49a
tsubasa ooishi <pop331240283@icloud.com>
2018-07-17 18:36:31 +0900

    Finish 5-4

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '17a6d9a444681d9754d4e4f3a34ee4dff0a5bc2a5023ea62fa276037add3126e19b01da636d5949811c3adeeebaa91d9597c278360ad68457a1f652149fd9249'
